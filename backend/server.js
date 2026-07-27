97c8bdca29d4cb32620cb83cf3f5123aca1acafe
Jagadeeshpasalapudi <jagadeeshpasalapudi2008@gmail.com>
2026-07-27 12:58:01 +0530

    Fix CORS allowed origins

--- backend/server.js
+++ backend/server.js
@@ -18,11 +18,13 @@ const app = express();
 
 app.use(
   cors({
-    origin: process.env.CLIENT_URL || "http://localhost:5173",
+    origin: [
+      "http://localhost:5173",
+      "https://medical-connect-seven.vercel.app",
+    ],
     credentials: true,
   }),
 );
-
 app.use(express.json());
 
 app.use(express.urlencoded({ extended: true }));
